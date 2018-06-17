2a971df2c353db9564ad491c91a2337b06e27989
Hameet <hameet.singh@hotmail.com>
2018-06-17 19:31:03 +1200

    add store

--- src/components/Square/index.spec.js
+++ src/components/Square/index.spec.js
@@ -6,6 +6,12 @@ import Square from '.'
 describe('components:Square', () => {
   it('renders the Square with the proper styles for player O in the top left square', () => {
     expect(
+      toJson(shallow(<Square handleClick={() => null} index={0} />).dive())
+    ).toMatchSnapshot()
+  })
+
+  it('renders the Square with the proper styles for player X in the top left square', () => {
+    expect(
       toJson(shallow(<Square player='o' index={0} />).dive())
     ).toMatchSnapshot()
   })
